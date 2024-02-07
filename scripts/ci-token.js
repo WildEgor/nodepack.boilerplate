efaf0c8acdffd273b6f75f8fceba701de6a8c736
Egor Kartashov <kartashov_egor96@mail.ru>
2024-02-07 12:04:49 +0600

    fix: gh action

--- scripts/ci-token.js
+++ scripts/ci-token.js
@@ -2,16 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 const placeholder = '%TOKEN%';
+const orgPlaceholder = '%ORG%';
 const templateLines = [
   'registry=https://registry.npmjs.org/',
-  '@wildegor:registry=https://npm.pkg.github.com/',
+  `@${orgPlaceholder}:registry=https://npm.pkg.github.com/`,
   `//npm.pkg.github.com/:_authToken=${placeholder}`,
   '',
 ];
 
 (() => {
   const npmPath = path.resolve('.npmrc');
-  const npmContent = templateLines.join('\n').replace(placeholder, process.env.NODE_ENV_TOKEN.trim());
+  const npmContent = templateLines
+    .join('\n')
+    .replace(placeholder, process.env.NODE_ENV_TOKEN.trim())
+    .replace(orgPlaceholder, process.env.ORGANIZATION.trim());
   fs.writeFileSync(npmPath, npmContent);
   console.log(npmContent);
 })();
